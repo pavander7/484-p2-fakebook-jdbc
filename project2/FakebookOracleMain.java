9df9a204e684407b3af6b05e696fa944b0e7c5e6
pavander <pavander@umich.edu>
2025-02-12 16:39:37 -0500

    credentials

--- project2/FakebookOracleMain.java
+++ project2/FakebookOracleMain.java
@@ -8,8 +8,8 @@ import java.sql.SQLException;
 
 public final class FakebookOracleMain {
     // Member Variables
-    private static String username = "uniqname"; // replace with your uniqname
-    private static String password = "password"; // replace with your Oracle password (default: eecsclass)
+    private static String username = "pavander"; // replace with your uniqname
+    private static String password = "eecsclass"; // replace with your Oracle password (default: eecsclass)
 
     // [Main Function]
     // Expected Command Line Arguments:
